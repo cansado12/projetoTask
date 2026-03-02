65c589a9b80082d9522afb146a681900c6dc0883
David Alves <loucobr882@gmail.com>
2026-03-02 19:24:06 -0300

    primeiro commit

---
+++ src/main/java/com/br/cansado/projetoTask/ProjetoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.br.cansado.projetoTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/cansado/projetoTask/ProjetoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cansado.projetoTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
