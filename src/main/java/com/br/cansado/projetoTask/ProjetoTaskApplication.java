0e96e5ef7a35ba7d9c04430ab56f2c95806783bf
David Alves <loucobr882@gmail.com>
2026-03-14 21:47:27 -0300

    e

--- src/main/java/com/br/cansado/projetoTask/ProjetoTaskApplication.java
+++ src/main/java/com/br/cansado/projetoTask/ProjetoTaskApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ProjetoTaskApplication {
 
 	public static void main(String[] args) {
